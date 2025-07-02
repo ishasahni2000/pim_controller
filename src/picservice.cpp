58885d857b7e503ab7647fb69f60c0fb4715023c
Isha Sahni <ishasahni2000@gmail.com>
2025-07-02 13:57:56 -0500

    Initial commit: PIM Controller skeleton

---
+++ src/picservice.cpp
@@ -0,0 +1,20 @@
+#include <sdbusplus/bus.hpp>
+#include <iostream>
+
+int main()
+{
+    auto bus = sdbusplus::bus::new_default();
+    bus.request_name("xyz.openbmc_project.PICService");
+
+    std::cout << "PICService started. (Dummy for now)\n";
+
+    // This is a placeholder.
+    while (true)
+    {
+        bus.process_discard();
+        bus.wait();
+    }
+
+    return 0;
+}
+
